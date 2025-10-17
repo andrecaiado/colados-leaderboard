5f71f81da6edd57d5b74bc688c5d83e831defa34
andrecaiado <andrecaiado@gmail.com>
2025-10-17 15:59:11 +0100

    create test for findPlayerByCharacterAtOrBeforeDate

---
+++ colados-leaderboard-api/src/test/java/com/example/colados_leaderboard_api/repository/PlayerRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.example.colados_leaderboard_api.repository;
+
+import com.example.colados_leaderboard_api.entity.Player;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(scripts = "/test-data/player-data.sql")
+public class PlayerRepositoryTest {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    static Stream<Arguments> playerTestArguments1() {
+        return Stream.of(
+                Arguments.of("Mario", Instant.now(), 101),
+                Arguments.of("Toadette", Instant.parse("2025-04-10T09:02:00Z"), 102),
+                Arguments.of("Mario", Instant.parse("2025-06-05T11:02:00Z"), 101)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("playerTestArguments1")
+    void givenValidCharacterAndValidDate_whenFindPlayerByCharacterAtOrBeforeDate_thenReturnPlayer(String character, Instant date, int expectedUserId) {
+        Optional<Player> player = playerRepository.findPlayerByCharacterAtOrBeforeDate(character, date);
+        assert(player.isPresent());
+        assert(player.get().getCharacterName().equals(character));
+        assert(player.get().getCreatedAt().isBefore(date) || player.get().getCreatedAt().equals(date));
+        assert(player.get().getUserId() == expectedUserId);
+    }
+
+    static Stream<Arguments> playerTestArguments2() {
+        return Stream.of(
+                Arguments.of("Mario2", Instant.now()),
+                Arguments.of("Mario", Instant.parse("2024-06-05T11:02:00Z"))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("playerTestArguments2")
+    void givenInvalidCharacterOrDate_whenFindPlayerByCharacterAtOrBeforeDate_thenReturnEmpty(String character, Instant date) {
+        Optional<Player> player = playerRepository.findPlayerByCharacterAtOrBeforeDate(character, date);
+        assert(player.isEmpty());
+    }
+}
---
+++ colados-leaderboard-api/src/test/resources/test-data/player-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Player (user_id, character_name, created_at) VALUES
+    (101, 'Mario', '2025-10-01T10:00:00Z'),
+    (101, 'Yoshi', '2025-08-05T12:00:00Z'),
+    (101, 'Mario', '2025-06-05T11:00:00Z'),
+    (101, 'Toadette', '2025-04-10T09:00:00Z'),
+    (102, 'Toadette', '2025-04-10T09:01:00Z');
