445ed203a1f426fc3565a492c3b121d25ae67146
andrecaiado <andrecaiado@gmail.com>
2025-12-11 14:58:49 +0000

    fix migration file

--- colados-leaderboard-api/src/main/resources/db/migration/V6__alter_app_user_roles_constraints.sql
+++ colados-leaderboard-api/src/main/resources/db/migration/V6__alter_app_user_roles_constraints.sql
@@ -1,7 +1,13 @@
+-- Drop existing constraints if they exist
 ALTER TABLE app_user_roles
-DROP CONSTRAINT IF EXISTS fk_appuser_roles_on_app_user,
-DROP CONSTRAINT IF EXISTS fk_app_user_roles_app_user_id,
-ADD CONSTRAINT fk_app_user_roles_app_user_id
-FOREIGN KEY (app_user_id)
-REFERENCES app_user(id)
-ON DELETE CASCADE;
+    DROP CONSTRAINT IF EXISTS fk_appuser_roles_on_app_user;
+
+ALTER TABLE app_user_roles
+    DROP CONSTRAINT IF EXISTS fk_app_user_roles_app_user_id;
+
+-- Add the correct constraint with ON DELETE CASCADE
+ALTER TABLE app_user_roles
+    ADD CONSTRAINT fk_app_user_roles_app_user_id
+        FOREIGN KEY (app_user_id)
+            REFERENCES app_user(id)
+            ON DELETE CASCADE;
